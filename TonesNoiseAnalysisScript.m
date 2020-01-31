e049d653b77263b2c9365e479123eb7dd091f0d7
Kelson <k.shilling-scrivo@gmail.com>
2020-01-30 20:15:52 -0500

    ExtractChannelsFix
    Added option to skip 2-channel data

---
+++ TonesNoiseAnalysisScript.m
@@ -0,0 +1,11 @@
+%TonesNoiseActive Analysis Script
+
+%Active GC
+GCanalTrialsModBalanced_TN(TonesNoiseActive(:,2),'Active')
+GCanalTrialsModBalanced_TN(TonesNoiseActive(:,2),'HitMiss')
+
+%Passive GC
+GCanalTrialsModBalanced_TN(TonesNoiseActive(:,1),'Passive')
+
+
+
