4c7cb8e1486b34e5ea62148022f42956b8619253
Kelson <k.shilling-scrivo@gmail.com>
2020-08-14 21:05:52 -0400

    Update PlotCellClusters.m

--- Misc_plotting/PlotCellClusters.m
+++ Misc_plotting/PlotCellClusters.m
@@ -10,11 +10,11 @@ function out = PlotCellClusters(Passive,manual_clust_ids,manual_colors)
 %TODO- Currently an issue where the rings are not stored correctly from the
 %CellIDS celldefinitionGUI plotting with just cell centers for now. would
 %need to regenerate missing roi boundaries to fix this issue. 
-
+out = struct();
     if exist('manual_colors','var')
         colors = manual_colors;
     else 
-        % colors = {'k','r','o','y','c','b'};for reference 
+        % colors = {'w','r','o','y','c','b'};for reference 
         colors = {[1 1 1 ],[1 0 0],[1 .5 0], [1 1 0],[0 1 1] ,[0 0 1]};
    
         
@@ -62,31 +62,23 @@ for expt_idx = 1:length(expts)
      elseif length(xpts_all) < length(classes)
          classes = classes(1:length(xpts_all));
      end 
-  %KNN Analysis
-  responsive_idx = classes ~=0;
-  classes = classes(responsive_idx);
-  xpts_all = xpts_all(responsive_idx);
-  ypts_all = ypts_all(responsive_idx);
- [out.KNNs{expt_num},out.Distances{expt_num} ] = knnsearch(xpts_all,...
-                                               ypts_all,'K',10);
-  out.KNN_classes{expt_num} =  classes(out.KNNs{expt_num});
-   
-       
+  
     
     
     % these will load the avg_image as avgGreenFilteredImage
-    try
-        load(fullfile(expt_directory,'AvgImg.mat'));
-   
-    catch
-        try
+ try
         load(fullfile(expt_directory,'AvgGreenImage.mat'));
+        
         catch
             % create an average from raw image 
-%             f = fullfile(expt_directory,'GreenChannelRegistered.mat')
-%             
+             f = fullfile(expt_directory,'GreenChannelRegistered.mat');
+           img =fread(fopen(f,'r+'), 512*512*100,'uint16=>uint16');fclose(f);
+          
+           img = reshape(img,512,512,100);
+           avgGreenFilteredImage = squeeze(nanmean(img,3));
+           clear img
         end 
-    end 
+    
     
   
    
@@ -96,7 +88,10 @@ for expt_idx = 1:length(expts)
  % plot the average image 
     figure
       title(sprintf('Field #%s', expt_num)); 
-     try 
+     
+      
+      
+      try 
          imagesc(avgGreenfilteredImage)
          clear avgGreenfilteredImage
      catch
@@ -108,11 +103,11 @@ for expt_idx = 1:length(expts)
               expt_idx,Passive.Expt_names{expt_num});
     title(title_text,'Interpreter','None');
     
- for current_cluster = 1:max(classes) % classes are 0-indexed 
+ for current_cluster = 1:max(classes)+1 % classes are 0-indexed 
     
-     cells_to_plot = find(classes == current_cluster);
+     cells_to_plot = find(classes == current_cluster - 1);
      
-     cells_to_plot(cells_to_plot>length(cdef.ptsIdx)) =[];
+    % cells_to_plot(cells_to_plot>length(cdef.ptsIdx)) =[];
      
      xpts = cdef.ptsIdx(cells_to_plot,2);
      ypts = cdef.ptsIdx(cells_to_plot,3);
@@ -141,59 +136,71 @@ for expt_idx = 1:length(expts)
 %     end  
 %     
 %     
-    
-% gather KNN_classes 
-
-Histc= histcounts(...
-    out.KNN_classes{expt_num}(cells_to_plot,:),...
-    [.5:1:m+.5]) % edges 
-% normalize 
- out.Histc{expt_num,current_cluster}= Histc ./ sum(Histc);
- 
- % edge case for no neurons in cluster 
-if  isempty(out.Histc{expt_num,current_cluster})||...
-        any(isnan(out.Histc{expt_num,current_cluster}))
-    out.Histc{expt_num,current_cluster}= zeros(1,m);
-end 
+  
         
 
  end 
- 
+ print(gcf,'-dpsc','-append','\\vault3\data\kelson\CellClusters.ps')
 % 
 %  figure
 %   bar(cell2mat(out.Histc(expt_num,:)'),'stacked')
 %  
  end
 
+% 
+% 
 
-
-Sum_Histc = cell(1,m);
-
-for expt_idx = 1:length(out.Histc)
-    for class_idx = 1:m;
-        if size(out.Histc{1,class_idx},2) == m
-            
-            if isempty(Sum_Histc{1,class_idx})
-               Sum_Histc{1,class_idx} = out.Histc{1,class_idx}
-            else 
-                Sum_Histc{1,class_idx} = ...
-                    Sum_Histc{1,class_idx} +...
-                    out.Histc{1,class_idx};
-            end 
-        end 
-    end 
-end 
 % for ii = 1: m            
 %     figure
 %         bar(Sum_Histc{ii})
 %     title(sprintf('KNN of cluster %d',ii));
 % end 
 %    
-end 
 
-
-
-
+% 
+% 
+% %KNN Analysis
+%   responsive_idx = classes ~=0;
+%   classes = classes(responsive_idx);
+%   xpts_all = xpts_all(responsive_idx);
+%   ypts_all = ypts_all(responsive_idx);
+%  [out.KNNs{expt_num},out.Distances{expt_num} ] = knnsearch(xpts_all,...
+%                                                ypts_all,'K',10);
+%   out.KNN_classes{expt_num} =  classes(out.KNNs{expt_num});
+%    
+%     
+% % gather KNN_classes 
+% 
+% Histc= histcounts(...
+%     out.KNN_classes{expt_num}(cells_to_plot,:),...
+%     [.5:1:m+.5]) % edges 
+% % normalize 
+%  out.Histc{expt_num,current_cluster}= Histc ./ sum(Histc);
+%  
+%  % edge case for no neurons in cluster 
+% if  isempty(out.Histc{expt_num,current_cluster})||...
+%         any(isnan(out.Histc{expt_num,current_cluster}))
+%     out.Histc{expt_num,current_cluster}= zeros(1,m);
+% end 
+%   
+%   
+%        
+% end 
+% Sum_Histc = cell(1,m);
+% 
+% for expt_idx = 1:length(out.Histc)
+%     for class_idx = 1:m
+%         if size(out.Histc{1,class_idx},2) == m
+%             
+%             if isempty(Sum_Histc{1,class_idx})
+%                Sum_Histc{1,class_idx} = out.Histc{1,class_idx}
+%             else 
+%                 Sum_Histc{1,class_idx} = ...
+%                     Sum_Histc{1,class_idx} +...
+%                     out.Histc{1,class_idx};
+%             end 
+%         end 
+%     end 
 
 
   
