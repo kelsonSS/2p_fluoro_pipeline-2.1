4c2a0d535a8918537706a4b190d6768d0719849c
Kelson <k.shilling-scrivo@gmail.com>
2020-08-14 21:05:34 -0400

    Update Corrs_by_temporal_timing.m

--- Corrs_by_temporal_timing.m
+++ Corrs_by_temporal_timing.m
@@ -3,20 +3,38 @@ function  Corrs = Corrs_by_temporal_timing(TN)
 % the values for different subsets of neurons ( currently written to be
 % four sets named below)
 
-Names = {'All','Noise','Tone_on','Tone_off'};
+if isfield(TN,'Class_idx')
+    m = max(TN.Clust_num);
+   
+    for idx = 1:m
+        Indicies{idx} = idx == TN.Class_idx;
+        Names{idx} = sprintf('Cluster %d',idx);
+    end 
+    
+    if isfield(TN,'Classes')
+        Names = TN.Classes;
+    end 
+    Corrs = cell(m,m);
+    
+else 
+Names = {'All','Noise','Tone_on','Tone_off','Noise_off'};
 DFF_mu = squeeze(nanmean(TN.DFF,2));
 [~,onsets]  = max(DFF_mu);
 
 Noise_idx = onsets > 30 & onsets <= 60;
 Tone_idx  = onsets > 60 & onsets <= 90;
 Offset_idx = onsets > 90 & onsets<= 120;
+Off_idx = onsets > 120;  
 
-Indicies = {logical(onsets);Noise_idx;Tone_idx;Offset_idx};
+Indicies = {logical(onsets);Noise_idx;Tone_idx;Offset_idx;Off_idx};
 Corrs = cell(4,2);
-
 m = length(Indicies);
-for subgroup1 = 1:4
-    for subgroup2 = 1:4
+
+end 
+
+
+for subgroup1 = 1:m
+    for subgroup2 = 1:m
         if subgroup1>subgroup2
             % upper triangular form
             continue
@@ -31,7 +49,11 @@ for subgroup1 = 1:4
     TN_temp.experiment_list = TN.experiment_list(combined_index);
     TN_temp.active = TN.active(combined_index,:);
     
-    
+   
+   % could consider subsetting, but since fields are broken up by trial it 
+   % may require more work on the correlation function itself to show which
+   % neurons are in each group
+   
     Corrs{subgroup1,subgroup2} = Correlations(TN_temp);
       Corrs{subgroup1,m+1} = group_name;
     
