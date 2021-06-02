dfe07d00e2619abb0bc97e828546e57c07403b0c
Jones812 <mittelnote@gmail.com>
2021-06-02 17:01:46 -0400

    Jonah fixes

--- AnalysisScripts/JKMAfferent.m
+++ AnalysisScripts/JKMAfferent.m
@@ -1,22 +1,20 @@
-% %JKM Processing Script
-% %Get Data Dirs
-% dataDir=ans.DataDirs;
-% 
-% %%
-% %%Fluoro_to_Table
-% Fluoro_to_Table(dataDir)
-% DF=ans;
+%JKM Processing Script
+%Get Data Dirs
+dataDir=DF.DataDirs;
+
+%%
+%%Fluoro_to_Table
+Fluoro_to_Table(dataDir)
+DF=ans;
 %%
 %%Get Clusters
 plotAllDFFs(DF)
 Cluster_DF(DF)
 
-%%
-%%Get Bandwidth
+%
+%Get Bandwidth
 BandwidthAnalysis(DF, 'Significant',0,'Pos',.5)
 BandwidthData=ans
-% oldBandwidthAnalysis(DF, 'Significant',0,'Pos',.5)
-% oldBandwidthAnalysis(DF, 'Significant',0,'Neg',.5)
 
 %%
 %%Get Cell Response Timing
